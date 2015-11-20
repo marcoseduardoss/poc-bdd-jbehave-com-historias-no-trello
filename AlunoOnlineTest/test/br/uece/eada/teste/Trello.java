e69f82f2d9862c4e5cb4862a0c9a0e683a4ee8fb
Marcos Eduardo <marcoseduardoss@gmail.com>
2015-11-19 22:18:43 -0300

    Change-Id: I82a88ab45d6447ee48431f892914f4fcff22f39a

--- AlunoOnlineTest/test/br/uece/eada/teste/Trello.java
+++ AlunoOnlineTest/test/br/uece/eada/teste/Trello.java
@@ -2,7 +2,7 @@ package br.uece.eada.teste;
 
 public class Trello {
 
-	public static final String PATH_PHANTOMJS = "/home/novajoc/phantomjs";	// inicializa dados de acesso ao seleinum
+	public static final String PATH_PHANTOMJS = "/home/novajoc/public_ftp/phantomjs";	// inicializa dados de acesso ao seleinum
 //	public static final String PATH_PHANTOMJS = "C:\\marcos\\phantomjs.exe";
 	public static final String URL 		= "file:///C:/projetos/java/EADA/TELAS_EADA";
 	public static final String TOKEN 	= "a5948f990dce6320b68739410ee0ca92b57956233f7cc1a1daaa6ef47feb01f0";
