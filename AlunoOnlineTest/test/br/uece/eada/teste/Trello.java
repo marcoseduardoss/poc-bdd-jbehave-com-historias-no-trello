7a02528b1a224376c63a178d40c7bb2e32dbc228
marcoseduardoss <marcoseduardoss@gmail.com>
2016-01-07 20:38:55 -0300

    Update Trello.java
    alteração de verificação de valores no windows e no linux

--- AlunoOnlineTest/test/br/uece/eada/teste/Trello.java
+++ AlunoOnlineTest/test/br/uece/eada/teste/Trello.java
@@ -2,10 +2,9 @@ package br.uece.eada.teste;
 
 public class Trello {
 
-	public static final String PATH_PHANTOMJS = "/usr/bin/phantomjs";	// inicializa dados de acesso ao seleinum
-//	public static final String PATH_PHANTOMJS = "C:\\marcos\\phantomjs.exe";
-//	public static final String URL 		= "file:///C:/projetos/java/EADA/TELAS_EADA";
-	public static final String URL 		= "http://93.188.161.196:8080/AlunoOnlineTest";
+	private static boolean isWindows = System.getProperty("os.name").toLowerCase().contains("windows");
+	public static final String PATH_PHANTOMJS = isWindows ? "C:\\marcos\\phantomjs.exe":"/usr/bin/phantomjs";
+	public static final String URL 		= isWindows ? "http://localhost:8080/AlunoOnlineTest":"http://93.188.161.196:8080/AlunoOnlineTest";
 	public static final String TOKEN 	= "a5948f990dce6320b68739410ee0ca92b57956233f7cc1a1daaa6ef47feb01f0";
 	public static final String KEY 		= "9d31db58c7b43cb5b11e0832df3a6bc2";
 }
