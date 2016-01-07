efe76c842cea59e732e5a2f340bb894523a549e7
marcoseduardoss <marcoseduardoss@gmail.com>
2016-01-07 16:51:58 -0300

    Update Trello.java
    alteração

--- AlunoOnlineTest/test/br/uece/eada/teste/Trello.java
+++ AlunoOnlineTest/test/br/uece/eada/teste/Trello.java
@@ -5,7 +5,7 @@ public class Trello {
 	public static final String PATH_PHANTOMJS = "phantomjs";	// inicializa dados de acesso ao seleinum
 //	public static final String PATH_PHANTOMJS = "C:\\marcos\\phantomjs.exe";
 //	public static final String URL 		= "file:///C:/projetos/java/EADA/TELAS_EADA";
-	public static final String URL 		= "http://novajo.com.br/AlunoOnlineTest";
+	public static final String URL 		= "http://93.188.161.196:8080/AlunoOnlineTest";
 	public static final String TOKEN 	= "a5948f990dce6320b68739410ee0ca92b57956233f7cc1a1daaa6ef47feb01f0";
 	public static final String KEY 		= "9d31db58c7b43cb5b11e0832df3a6bc2";
 }
